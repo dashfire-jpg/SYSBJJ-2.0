bdf287af849b875a9083ad3f62c49abdbee67340
dashfire-jpg <dashfire@gmail.com>
2026-04-21 20:02:49 -0300

    Change error message language to Portuguese
    Updated error message to Portuguese for better localization.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Root não encontrado");
 }
 
 ReactDOM.createRoot(rootElement).render(
