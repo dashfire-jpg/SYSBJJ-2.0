21277ea4bacf139332bf04d4ea11d3098795be20
dashfire-jpg <dashfire@gmail.com>
2026-04-21 20:05:11 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./index.css";
+
+import { LanguageProvider } from "./contexts/LanguageContext";
+import { DataProvider } from "./contexts/DataContext";
 
 const rootElement = document.getElementById("root");
 
@@ -13,7 +17,11 @@ if (!rootElement) {
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <LanguageProvider>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </LanguageProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
