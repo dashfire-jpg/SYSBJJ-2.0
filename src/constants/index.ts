0377e056a496a5e3ba3913192b96a0416240dfef
dashfire-jpg <dashfire@gmail.com>
2026-04-21 09:28:27 -0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,19 +1,21 @@
-import { IBJJF_LESSONS as RAW_IBJJF_LESSONS } from "./rulesData";
-import { BELT_COLORS as RAW_BELT_COLORS } from "./beltColors";
+export { BELT_COLORS } from "./beltColors";
+export { IBJJF_LESSONS } from "./rulesData";
 
-export const BELT_COLORS = RAW_BELT_COLORS ?? {};
+// Regras de faixa IBJJF (CORRIGIDO E ÚNICO)
+export const IBJJF_BELT_RULES = {
+  kids: {
+    white: { minAge: 4, maxAge: 15 },
+    gray: { minAge: 4, maxAge: 15 },
+    yellow: { minAge: 7, maxAge: 15 },
+    orange: { minAge: 10, maxAge: 15 },
+    green: { minAge: 13, maxAge: 15 },
+  },
 
-export const IBJJF_LESSONS = RAW_IBJJF_LESSONS ?? {};
-
-export const IBJJF_BELT_RULES =
-  RAW_IBJJF_LESSONS?.IBJJF_BELT_RULES ?? {
-    kids: {
-      white: { minAge: 4, maxAge: 15 },
-    },
-    adults: {
-      white: { minTime: 0 },
-      blue: { minTime: 2 },
-      purple: { minTime: 5 },
-      brown: { minTime: 1 },
-    },
-  };
+  adults: {
+    white: { minTime: 0 },
+    blue: { minTime: 2 },
+    purple: { minTime: 5 },
+    brown: { minTime: 1 },
+    black: { minTime: 0 },
+  },
+};
