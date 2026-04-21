9f3af456e6ecd608e30da0d94a73844dec3114ac
dashfire-jpg <dashfire@gmail.com>
2026-04-21 08:37:29 -0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,14 @@ export const THEME = {
   primary: "#0f172a",
   secondary: "#1e293b",
 };
+export const IBJJF_BELT_RULES = {
+  kids: {
+    white: { minAge: 4, maxAge: 15 },
+  },
+  adults: {
+    white: { minTime: 0 },
+    blue: { minTime: 2 },
+    purple: { minTime: 5 },
+    brown: { minTime: 1 },
+  }
+};
